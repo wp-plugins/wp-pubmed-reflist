d8c7e9ed7c3f57e32d2bf3b3d0a5bdb6321f0149
jimhu <jimhu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-27 16:18:09 +0000

    WP PubMed Reflist version 0.7
    git-svn-id: https://plugins.svn.wordpress.org/wp-pubmed-reflist/trunk@1076763 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- class.wpPubMedRefListSettings.php
+++ class.wpPubMedRefListSettings.php
@@ -27,34 +27,145 @@ revised structure for the options array as a single wp-options property
 
 # calling the function to create the object defers construction until the rest of WP codex has loaded.
 add_action('admin_menu', 'pubmed_refs_admin_add_page');
+
 function pubmed_refs_admin_add_page() {
 	$wpPubMedRefListSettingsPage = new wpPubMedRefListSettings;
 	/*
-	Add an item to the Settings menu on the admin page
+	pre 0.7 Add an item to the Settings menu on the admin page
 	
 	add_options_page($page_title, $menu_title, $capability, $menu_slug, $function);
 	menu slug is a url-friendly version of the menu name. Used as the "page name" in other functions.
+	
+	post 0.7 Add an item to the Tools menu so Editors can see it
+	add_management_page( $page_title, $menu_title, $capability, $menu_slug, $function );
+	
+	note that the wpPubMedRefListSettings constructor also modifies permissions to allow Editors to save.
 	*/
 
-	add_options_page(
+	add_management_page(
 		'WP PubMed Reflist', 	# page title
 		'WP PubMed Reflist', 	# menu title
-		'manage_options',    	#capability
+		'edit_posts',    	#capability
 		'wp_pubmed_reflist', 	#menu_slug
 		array($wpPubMedRefListSettingsPage, 'options_page') # callback function
 	);
 }
-
-
 
 class wpPubMedRefListSettings{
 
 	private $options = array();
 	
 	function __construct(){
+		# set callback for initializing admin pages
 		add_action('admin_init', array($this, 'admin_init'));
-	
+		# Allow Editor Role to edit settings
+		update_user_option( get_current_user_id(), 'manage_options', true, false );	
 	}
+
+	/*
+	Initalization: define form sections and callbacks.
+	
+	add_settings_section( $id, $title, $callback, $page )
+	add_settings_field( $id, $title, $callback, $page, $section, $args )
+	
+	*/
+	
+	function admin_init(){
+		global $new_whitelist_options;
+		$this->options = get_option('wp_pubmed_reflist');
+		$this->formats = get_option('wp_pubmed_reflist_styles');
+		# set default style to NIH if it isn't set
+		if(!isset($this->formats['default_style'])) $this->formats['default_style'] = 'NIH';
+
+		/*
+		Registry for query managment tab
+		*/
+		register_setting( 
+			'pubmed_refs_options',                   # option group to be called by settings_fields() in form generation
+			'wp_pubmed_reflist',                     # option name
+			array($this, 'pubmed_queries_validate')  # sanitize callback
+			);
+		# add new section for adding keys
+		add_settings_section(
+			'wp_pubmed_reflist_faclist',          	
+			'New key', 
+			'wpPubMedReflistViews::faclist_text', 
+			'wp_pubmed_reflist'
+			);
+		add_settings_field(
+			'wp_pubmed_reflist_faclist', 
+			'', array($this, 'faclist_form'), 
+			'wp_pubmed_reflist', 
+			'wp_pubmed_reflist_faclist'
+			);
+		# edit queries section
+		add_settings_section(
+			'wp_pubmed_reflist_qlist',      	# id
+			'Queries',                       	# title
+			'wpPubMedReflistViews::query_form_text',	# callback 
+			'wp_pubmed_reflist'               	# page
+			);
+		add_settings_field(
+			'wp_pubmed_reflist_qlist',        	# id
+			'',                              	# title
+			array($this,'query_form'),        	# callback
+			'wp_pubmed_reflist',             	# page
+			'wp_pubmed_reflist_qlist'         	# section
+			);
+	
+		/*
+		Registry for styles managment tab
+		*/
+	
+		register_setting( 
+			'pubmed_ref_styles',                    	# option group
+			'wp_pubmed_reflist_styles',                	# option name
+			array($this, 'pubmed_formats_validate')    	# sanitize callback
+			);
+		# edit formats
+		add_settings_section(
+			'wp_pubmed_styles',      	# id
+			'Add style',                       	# title
+			'wpPubMedReflistViews::styles_form_text',	# callback 
+			'wp_pubmed_reflist_styles'               	# page
+			);
+		add_settings_field(
+			'wp_pubmed_reflist_faclist',      	# id
+			'',                              	# title
+			array($this, 'styles_form'),      	# callback
+			'wp_pubmed_reflist_styles',        	# page
+			'wp_pubmed_styles'              	# section	 
+			);
+		add_settings_section(
+			'wp_pubmed_style_data_section',      	# id
+			'Style data',                       	# title
+			'wpPubMedReflistViews::styles_data_form_text',	# callback 
+			'wp_pubmed_reflist_styles'               	# page
+			);
+		add_settings_field(
+			'wp_pubmed_style_data',         	# id
+			'',                              	# title
+			array($this,'style_props_form'),   	# callback
+			'wp_pubmed_reflist_styles',        	# page
+			'wp_pubmed_style_data_section'              	# section
+			);
+		add_settings_section(
+			'wp_pubmed_itals',      	# id
+			'Italicized words',                       	# title
+			'wpPubMedReflistViews::styles_ital_form_text',	# callback 
+			'wp_pubmed_reflist_styles'               	# page
+			);
+		add_settings_field(
+			'wp_pubmed_style_italicize',         	# id
+			'',                              	# title
+			array($this,'styles_ital_form'),   	# callback
+			'wp_pubmed_reflist_styles',        	# page
+			'wp_pubmed_itals'              	# section
+			);
+	}
+
+
+
 
 	/*
 	This is the wrapper HTML form displayed when the Settings menu is called
@@ -67,73 +178,91 @@ class wpPubMedRefListSettings{
 	*/
 	function options_page(){
 		global $new_whitelist_options;
-		echo "<div>
-		<h2>PubMed Reflist</h2>
-		Manage PubMed queries for use with the [pmid-refs] shorttag.
-		<form action='options.php' method='post'>";
-		
+		echo "<h2>PubMed Reflist</h2>
+		Manage PubMed queries for use with the [pmid-refs key=<key> limit=<number>] shorttag.";
+		echo 
+"<div class='wrap'>";
+		/*
+		Tabbed management
+		modified from 		http://code.tutsplus.com/tutorials/the-complete-guide-to-the-wordpress-settings-api-part-5-tabbed-navigation-for-your-settings-page--wp-24971
+		*/
+		$active_tab = 'query';
+		if( isset( $_GET[ 'tab' ] ) ) {
+    		$active_tab = $_GET[ 'tab' ];
+		} // end if
+		$query_tab_class = $format_tab_class = $help_tab_class = 'nav-tab';
+		$active_tab_class = $active_tab.'_tab_class';
+		$$active_tab_class = 'nav-tab-active';	
+		echo "
+		<h2 class='nav-tab-wrapper'>
+			<a href='?page=wp_pubmed_reflist&tab=query' class='$query_tab_class'>Queries</a>
+			<a href='?page=wp_pubmed_reflist&tab=format' class='$format_tab_class'>Reference Styles</a>
+			<a href='?page=wp_pubmed_reflist&tab=help' class='$help_tab_class'>Help</a>
+		</h2>";
+		echo "<form action='options.php' method='post'>";
+		switch($active_tab){
+			case 'help':
+				wpPubmedReflistViews::help();
+				break;
+			case 'format':
+				settings_fields('pubmed_ref_styles'); 
+				do_settings_sections('wp_pubmed_reflist_styles');
+				submit_button('Save Changes','submit', true, array('tab'=>'format'));
+				break;
+			case 'query':
+			default:
 		settings_fields('pubmed_refs_options'); 
 		do_settings_sections('wp_pubmed_reflist');
-		# not sure what the esc_attr_e function does.  Copying from tutorial
-		echo "<input name='Submit' type='submit' value='";esc_attr_e('Save Changes'); echo " ' /></form></div>";	
-		return;
+				submit_button('Save Changes','submit', true, array('tab'=>'query'));
 	}
+		echo "</form></div>";
 	
-	/*
-	Initalization: define form sections and callbacks.
-	
-	add_settings_section( $id, $title, $callback, $page )
-	add_settings_field( $id, $title, $callback, $page, $section, $args )
-	
-	*/
-	
-	function admin_init(){
-		global $new_whitelist_options;
-		$this->options = get_option('wp_pubmed_reflist');
-		register_setting( 'pubmed_refs_options', 'wp_pubmed_reflist', array($this, 'pubmed_queries_validate') );
-		# main query section
-		add_settings_section('wp_pubmed_reflist_qlist', 'Queries', array($this, 'query_form_text'), 'wp_pubmed_reflist');
-		add_settings_field('wp_pubmed_reflist_qlist', '', array($this,'query_form'), 'wp_pubmed_reflist', 'wp_pubmed_reflist_qlist');
-	
-		# add new section
-	#	register_setting( 'pubmed_refs_options', 'wp_pubmed_reflist', array($this, 'pubmed_faclist_validate' ));
-		add_settings_section('wp_pubmed_reflist_faclist', 'New key', array($this, 'faclist_text'), 'wp_pubmed_reflist');
-		add_settings_field('wp_pubmed_reflist_faclist', '', array($this, 'faclist_form'), 'wp_pubmed_reflist', 'wp_pubmed_reflist_faclist');
+		return;
 	}	
 	
-	function query_form_text() {
-		echo '<p>Edit Queries. Queries can be any valid pubmed query, '.
-		'or you can use query keys as *key* and query text using || (two pipes) as an OR separator</p>'.
-		'<p>Extras is for citations of publications not listed in pubmed. Put formatted citations in this field, one citation per line</p>';
-	}
+	
 	
 	/*
 	Generate the forms for query editing
 	*/
 	function query_form(){
 		# get the previous value returns false if empty
-		$faclist = $this->options['faclist'];
-		$facprops = $this->options['facprops'];
-		#echo "<pre>options: "; var_dump($faclist);var_dump($facprops);echo "</pre>"; 	
+		$faclist = @$this->options['faclist'];
+		$facprops = @$this->options['facprops'];
+		#echo "<pre>options: \nfaclist\n"; var_dump($faclist);echo "facprops\n";var_dump($facprops);echo "</pre>"; 	
 		echo "<table><tr><th>Search Key</th><th>Query</th><th>Extras</th><th>Delete?</th></tr>";
 		if($faclist){
 			sort($faclist);
 			foreach($faclist as $name){
+				# sanitize the faclist name
+				$sanitizedName = preg_replace('/[^a-zA-Z0-9\s-_\(\)]/', '', $name);
+				if($sanitizedName != $name){
+					echo "$name contains characters not allowed in the key";
+					# delete it if it is already in the db.
+					if(($key = array_search($name, $this->options['faclist'])) !== false) {
+						unset($this->options['faclist'][$key]);
+					}
+					if(isset($facprops[$name]['query'])){
+						$facprops[$sanitizedName]['query'] = $facprops[$name]['query'];
+						unset ($facprops[$name]['query']);
+					}							
+					continue;
+				}
 				if ($name == '' || (isset($facprops[$name]['delete']) && $facprops[$name]['delete'] == 'on')) continue;
 				$name = strtolower($name);
 				$val = $extras = "";
 				echo "<tr><td>$name</td>";
-
+				# query
 				if(isset($facprops[$name]['query'])) $val = $facprops[$name]['query'];
 				echo "<td><textarea id='wp_pubmed_reflist_qlist' ".
 				"name='wp_pubmed_reflist[facprops][$name][query]' 
 				cols='60' rows='2' >$val</textarea></td>";
-
+				# extra citations not in pubmed
 				if(isset($facprops[$name]['extras'])) $extras = $facprops[$name]['extras'];
 				echo "<td><textarea id='wp_pubmed_reflist_qlist' ".
 				"name='wp_pubmed_reflist[facprops][$name][extras]' 
 				cols='60' rows='2' >$extras</textarea></td>";
-				
+				# delete?
 				echo "<td><input type='checkbox' id='wp_pubmed_reflist_qlist' ".
 				"name='wp_pubmed_reflist[facprops][$name][delete]' /></td>
 				<tr>";
@@ -145,43 +274,201 @@ class wpPubMedRefListSettings{
 		echo "</table>";
 	}
 	
-	function faclist_text() {
-		echo '<p>Add new search key.  Search keys are used to tell the shortcode what query '.
-		'to use and can be almost any arbitrary unquoted text. We use names of faculty members, '.
-		'or mnemonics for searches like "recent"</p>';
-	}
 	
+	/*
+	The form that adds new faculty gets submitted each time whether or not there is anything new
+	We use hidden input fields to make sure we keep all the old values
+	*/
 	function faclist_form(){
 		$faclist = array();
-		$facprops = $this->options['facprops'];
+		$facprops = @$this->options['facprops'];
 		if($this->options && isset($this->options['faclist']) ) $faclist = $this->options['faclist'];
 		#$faclist = get_option('wp_pubmed_reflist_faclist');
 		#echo "<pre>options: "; var_dump($this->options);echo "</pre>"; 	
-		echo "<table>";
 		$i = 0;
-		sort($faclist);
+		#sort($faclist);
 		foreach ($faclist as $name){
 			$name = strtolower($name);
 			if ($name != "" && !(isset($facprops[$name]['delete']) && $facprops[$name]['delete'] == 'on')){
-				#echo "$name<br>";
 				echo "<input id='wp_pubmed_reflist_faclist' name='wp_pubmed_reflist[faclist][$i]' type='hidden' value='{$name}' />";
 				$i++;
 			}
 		}		
-		echo "<tr><td>Add new:<input id='wp_pubmed_reflist' name='wp_pubmed_reflist[faclist][$i]' size='40' type='text' value='' /></td></tr>";
-		echo "</table>";
+		echo "Add new:<input id='wp_pubmed_reflist' name='wp_pubmed_reflist[faclist][$i]' size='40' type='text' value='' />";
 	
 	}
 	
+	function style_props_form(){
+		# get the previous value returns false if empty
+		$stylelist = $this->formats['stylelist'];
+		$styleprops = $this->formats['styleprops'];
+		echo "<table border='1'><tr><th style='width:auto;'>Default</th><th>Style Name</th><th>Format</th><th style='width:auto;'>Author limit</th><th style='width:auto;'>Delete?</th></tr>";
+		if($stylelist){
+			sort($stylelist);
+			foreach($stylelist as $name){
+				# sanitize the stylelist name
+				$sanitizedName = preg_replace('/[^a-zA-Z0-9\s-_\(\)]/', '', $name);
+				if($sanitizedName != $name){
+					echo "$name contains characters not allowed in the key";
+					# delete it if it is already in the db.
+					if(($key = array_search($name, $this->formats['stylelist'])) !== false) {
+						unset($this->formats['stylelist'][$key]);
+					}
+					if(isset($styleprops[$name]['format'])){
+						$styleprops[$sanitizedName]['format'] = $styleprops[$name]['format'];
+						unset ($styleprops[$name]['format']);
+					}							
+					continue;
+				}
+				if ($name == '' || (isset($styleprops[$name]['delete']) && $styleprops[$name]['delete'] == 'on')) continue;
+				#$name = strtolower($name);
+				$val = "";
+				
+				# radio buttons to set default style
+				$checked = '';
+				if(isset($this->formats['default_style']) && $this->formats['default_style'] == $name) $checked = 'checked';
+				echo "<tr>
+					<td><input id='wp_pubmed_reflist_stylelist' name='wp_pubmed_reflist_styles[default_style]' ".
+					"type='radio' value='$name' $checked/></td>";
+				echo "<td>$name</td>";
+				$template = '';
+				if(isset($styleprops[$name]['format'])) $template = $styleprops[$name]['format'];
+				
+				# max number of authors in author list
+				$authlimit = '';
+				if(isset($styleprops[$name]['authlimit'])) $authlimit = $styleprops[$name]['authlimit'];
+				# number of authors to show in author list if > $authlimit
+				$authshow = '';
+				if(isset($styleprops[$name]['authshow'])) $authshow = $styleprops[$name]['authshow'];
+	
+				if(in_array($name, $this->default_styles)){
+					echo "<td><input id='wp_pubmed_reflist_stylelist' name='wp_pubmed_reflist_styles[styleprops][$name][format]'". 
+					"type='hidden' value='$template' />".htmlentities($template)."</td>
+					<td>
+					<input id='wp_pubmed_reflist_stylelist' ".
+					"name='wp_pubmed_reflist_styles[styleprops][$name][authlimit]' 
+					type='hidden' value='$authlimit' />Limit: $authlimit<br>
+					<input id='wp_pubmed_reflist_stylelist' ".
+					"name='wp_pubmed_reflist_styles[styleprops][$name][authshow]' 
+					type='hidden' value='$authshow' />Show: $authshow
+					</td><td></td>";
+				}else{
+					# reference template
+					echo "<td><textarea id='wp_pubmed_reflist_stylelist' ".
+					"name='wp_pubmed_reflist_styles[styleprops][$name][format]' 
+					cols='60' rows='2' >$template</textarea></td>";
+					# author list limit
+					echo "<td>
+					Limit<input type='text' size='3' id='wp_pubmed_reflist_stylelist' ".
+					"name='wp_pubmed_reflist_styles[styleprops][$name][authlimit]' value='$authlimit' /><br>
+					Show<input type='text' size='3' id='wp_pubmed_reflist_stylelist' ".
+					"name='wp_pubmed_reflist_styles[styleprops][$name][authshow]' value='$authshow' />
+					</td>";
+					# delete?
+					echo "<td><input type='checkbox' id='wp_pubmed_reflist_stylelist' ".
+					"name='wp_pubmed_reflist_styles[styleprops][$name][delete]' /></td>
+					<tr>";
+					# last update
+					echo "<input id='wp_pubmed_reflist_stylelist'". 
+					"name='wp_pubmed_reflist_styles[styleprops][$name][last_update]' type='hidden' value='0' />";
+				}
+			}
+		}	
+		echo "</table>";
+	
+	}
+	/*
+	The form that adds new styles gets submitted each time whether or not there is anything new
+	We use hidden input fields to make sure we keep all the old values
+	*/
+	function styles_form(){
+		$this->set_default_styles();
+		$styleProps = $this->formats['styleprops'];
+		if($this->formats && isset($this->formats['stylelist']) ) $styleList = $this->formats['stylelist'];
+	
+		$i = 0;
+		foreach ($styleList as $name){
+			#$name = strtolower($name);
+			if ($name != "" && !(isset($styleProps[$name]['delete']) && $styleProps[$name]['delete'] == 'on')){
+				echo "<input id='wp_pubmed_reflist_styles' name='wp_pubmed_reflist_styles[stylelist][$i]' type='hidden' value='{$name}' />";
+				$i++;
+			}
+		}		
+		echo "Add new:<input id='wp_pubmed_reflist_formats' name='wp_pubmed_reflist_styles[stylelist][$i]' size='40' type='text' value='' />";
+	
+	}
+
+	/*
+	The form that adds new styles gets submitted each time whether or not there is anything new
+	We use hidden input fields to make sure we keep all the old values
+	*/
+	function styles_ital_form(){
+		$itals = '';
+		if($this->formats && isset($this->formats['itals']) ) $itals = $this->formats['itals'];
+		echo "<textarea id='wp_pubmed_reflist_stylelist' ".
+			"name='wp_pubmed_reflist_styles[itals]' 
+			cols='40' rows='10' >$itals</textarea>";	
+	}
+	
+	function set_default_styles(){
+		$this->default_styles = array('ASM','Cell', 'NIH','PNAS');
+		if (!in_array('ASM', $this->formats['stylelist']) ){
+			$this->formats['stylelist'][] = 'ASM';
+		}
+		$this->formats['styleprops']['ASM']['format'] = '<b>_Author</b>. _Year. _Title. _Journal <b>_Volume</b>:_Pages.';
+		$this->formats['styleprops']['ASM']['authlimit'] = 99;
+
+		if (!in_array('Cell', $this->formats['stylelist']) ){
+			$this->formats['stylelist'][] = 'Cell';
+		}
+		$this->formats['styleprops']['Cell']['format'] = '_Author (_Year). _Title. _Journal<i>_Volume</i>,_Pages.';
+		$this->formats['styleprops']['Cell']['authlimit'] = 10;
+
+		if (!in_array('NIH', $this->formats['stylelist']) ){
+			$this->formats['stylelist'][] = 'NIH';
+		}
+		$this->formats['styleprops']['NIH']['format'] = '_Author. _Title. _Journal. _Year;_Volume (_Issue):_Pages. _Epub. _DOI. _PMIDL _PMCL.';
+		$this->formats['styleprops']['NIH']['authlimit'] = 6;
+
+		if (!in_array('PNAS', $this->formats['stylelist']) ){
+			$this->formats['stylelist'][] = 'PNAS';
+		}
+		$this->formats['styleprops']['PNAS']['format'] = '_Author(_Year)_Title. <i>_Journal</i> _Volume (_Issue):_Pages.';
+		$this->formats['styleprops']['PNAS']['authlimit'] = 6;
+		$this->formats['styleprops']['PNAS']['authshow'] = 1;
+		
+		# set default italicization list
+		if($this->formats['itals'] == '' ){
+			$this->formats['itals'] = 
+				"in vivo\n".	
+				"in vitro\n".	
+				"in silico\n".	
+				"E. coli\n".	
+				"Escherichia coli\n".	
+				"B. subtilis\n".	
+				"Bacillus subtilis\n".	
+				"S. cerevisiae\n".	
+				"Saccharomyces cerevisiae\n"	
+			;
+		}
+		
+		update_option('wp_pubmed_reflist_styles', $this->formats);		
+
+
+	}
 	
 	/*
 	Input validation
 	TODO!
 	*/
 	function pubmed_queries_validate($text){
+	#	echo "is this callback used? $text";
 		return $text;
 	}
 	function pubmed_faclist_validate($text){
+		return $text;
+	}
+	function pubmed_formats_validate($text){
 		return $text;
 	}
 
